db43864b32e2346a1ea96858f3af0bf328584e3c
Jason-cs18 <jasonengineer@hotmail.com>
2022-12-05 13:42:13 +0800

    update instruction

--- Triton/triton_client.sh
+++ Triton/triton_client.sh
@@ -1,1 +1,6 @@
 # start a triton client application to send/receive a video
+# In default, $SCRIPTS is the path to $Turbo/Scripts/
+SCRIPTS=/home/jason/Toolbox/test_code/triton_client
+
+docker run -it --rm -v $SCRIPTS:/python_scripts \
+    --net host nvcr.io/nvidia/tritonserver:22.08-py3-sdk 
--- Triton/triton_server.sh
+++ Triton/triton_server.sh
@@ -4,5 +4,7 @@ MODEL_PATH=/home/jason/Toolbox/e2e-detection/temp/model_repository/
 
 # start triton server
 docker run --rm --gpus all -v $MODEL_PATH:/models \
+    -p8000:8000 -p8001:8001 -p8002:8002 \
     -e LD_PRELOAD=/models/libmmdeploy_tensorrt_ops.so \
-    nvcr.io/nvidia/tritonserver:22.08-py3 tritonserver --model-repository=/models/
+    nvcr.io/nvidia/tritonserver:22.08-py3 tritonserver --model-repository=/models/ \
+    --log-verbose=1
